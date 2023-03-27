a1971929dc4e7b77eeb574513116d02ce62b5fe5
Jozka Siregar <jozkasiregar@gmail.com>
2023-03-27 11:33:26 +0700

    unity intro pt.2

--- Praktek Unity Skilvul/Assets/Scripts/NewBehaviourScript.cs
+++ Praktek Unity Skilvul/Assets/Scripts/NewBehaviourScript.cs
@@ -4,14 +4,54 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    int playerHealth;
+    [SerializeField] private string playerName;
+    [SerializeField] private int playerHP;
+    [SerializeField] private int playerDamage;
+    [SerializeField] private GameObject playerFriend;
+    [SerializeField] private List<string> playerSkills;
+
+    void Awake()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        Debug.Log("Player Name: " + playerName);
+        Debug.Log("Total HP: " + playerHP);
+        Debug.Log("Total Damage" + playerDamage);
+        Debug.Log(playerFriend);
+    }
+
+    void FixedUpdate()
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Debug.Log("Hello Update!");
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+
+    void OnDisable()
+    {
+        
+    }
+
+    void OnDestroy()
     {
         
     }
