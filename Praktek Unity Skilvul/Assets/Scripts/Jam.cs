5f4e7aa60628bf21c5a0416076880d1b47652474
Jozka Siregar <jozkasiregar@gmail.com>
2023-03-28 11:43:58 +0700

    clock

---
+++ Praktek Unity Skilvul/Assets/Scripts/Jam.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jam : MonoBehaviour
+{
+    [SerializeField] private Transform pivotDetik;
+    [SerializeField] private Transform pivotMenit;
+    [SerializeField] private Transform pivotJam;
+    float speed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float detik = 0;
+        float menit = 0;
+        float jam = 0;
+
+        detik += speed * Time.deltaTime;
+        menit += (speed - 50) * Time.deltaTime;
+        jam += (speed - 80) * Time.deltaTime;
+
+
+        pivotDetik.Rotate(new Vector3(0, 0, detik));
+        pivotMenit.Rotate(new Vector3(0, 0, menit));
+        pivotJam.Rotate(new Vector3(0, 0, jam));
+    }
+}
